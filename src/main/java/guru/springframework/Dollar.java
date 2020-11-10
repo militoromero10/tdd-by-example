00515f9f7e6e59384d837d08aca740c314e4d70b
Camilo Romero <militoromero10@gmail.com>
2020-11-10 14:00:32 -0500

    prueba aceptada

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -4,8 +4,9 @@ public class Dollar {
     int amount;
 
     public Dollar(int amount) {
+        this.amount = amount;
     }
     void times(int multiplier){
-
+        amount *= multiplier;
     }
 }
